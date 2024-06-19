02713c9f62ff2eb752bc4be03920c1d0dd897ed7
Michael Antoš <128068126+antosmichael07@users.noreply.github.com>
2024-06-19 15:45:52 +0000

    loading disks from a directory

--- mispfs/main.go
+++ mispfs/main.go
@@ -8,19 +8,49 @@ import (
 var version = [3]byte{1, 0, 0}
 
 func main() {
-	if !CheckVersion(os.Args[1]) {
-		fmt.Printf("Disk doesn't have the same version as your read-writer\n")
-		return
+	disks, err := LoadDisks("disks")
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Printf("Good disk you have here\n")
+	for _, disk := range disks {
+		fmt.Printf("Disk: %s\n", disk.Name())
+	}
+}
+
+func LoadDisks(dir string) (disks []*os.File, err error) {
+	dir_contents, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Printf("Error reading disk directory\n")
+		return nil, err
+	}
+	var disk_names []string
+	for _, file := range dir_contents {
+		file_name := file.Name()
+		if file.IsDir() || file_name[len(file_name)-6:] == ".mpfs" || !CheckVersion(fmt.Sprintf("%s/%s", dir, file_name)) {
+			continue
+		}
+		disk_names = append(disk_names, file_name)
+	}
+
+	var valid_disks []*os.File
+	for _, disk := range disk_names {
+		file, err := os.Open(fmt.Sprintf("%s/%s", dir, disk))
+		if err != nil {
+			fmt.Printf("Error opening disk \"%s\"\n", disk)
+			continue
+		}
+		valid_disks = append(valid_disks, file)
+	}
+
+	return valid_disks, nil
 }
 
 func CheckVersion(disk string) bool {
 	data, err := os.ReadFile(disk)
 	if err != nil {
-		fmt.Printf("Error reading disk\n")
-		panic(err)
+		fmt.Printf("Error reading disk \"%s\"\n", disk)
+		return false
 	}
 
 	if len(data) < 3 {
