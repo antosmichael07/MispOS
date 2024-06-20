8a9e07b08bf2b0a593dec4ce37bc347cd5e687ea
Michael Antoš <128068126+antosmichael07@users.noreply.github.com>
2024-06-20 06:36:36 +0000

    added bootable disks

--- mispfs/main.go
+++ mispfs/main.go
@@ -13,41 +13,73 @@ func main() {
 		panic(err)
 	}
 
+	bootable_disks := GetBootableDisks(disks)
+
 	for _, disk := range disks {
 		fmt.Printf("Disk: %s\n", disk.Name())
+	}
+
+	fmt.Printf("\n")
+
+	for _, disk := range bootable_disks {
+		fmt.Printf("Bootable disk: %s\n", disk.Name())
 	}
 }
 
 func LoadDisks(dir string) (disks []*os.File, err error) {
 	dir_contents, err := os.ReadDir(dir)
+
 	if err != nil {
 		fmt.Printf("Error reading disk directory\n")
 		return nil, err
 	}
+
 	var disk_names []string
 	for _, file := range dir_contents {
 		file_name := file.Name()
+
 		if file.IsDir() || file_name[len(file_name)-6:] == ".mpfs" || !CheckVersion(fmt.Sprintf("%s/%s", dir, file_name)) {
 			continue
 		}
+
 		disk_names = append(disk_names, file_name)
 	}
 
-	var valid_disks []*os.File
 	for _, disk := range disk_names {
 		file, err := os.Open(fmt.Sprintf("%s/%s", dir, disk))
+
 		if err != nil {
 			fmt.Printf("Error opening disk \"%s\"\n", disk)
 			continue
 		}
-		valid_disks = append(valid_disks, file)
+
+		disks = append(disks, file)
 	}
 
-	return valid_disks, nil
+	return disks, nil
+}
+
+func GetBootableDisks(disks []*os.File) (bootable_disks []*os.File) {
+	for _, disk := range disks {
+		data := make([]byte, 1)
+		_, err := disk.Read(data)
+
+		if err != nil {
+			fmt.Printf("Error reading disk \"%s\"\n", disk.Name())
+			continue
+		}
+
+		if data[0] & 0b10000000 == 0b10000000 {
+			bootable_disks = append(bootable_disks, disk)
+		}
+	}
+
+	return bootable_disks
 }
 
 func CheckVersion(disk string) bool {
 	data, err := os.ReadFile(disk)
+
 	if err != nil {
 		fmt.Printf("Error reading disk \"%s\"\n", disk)
 		return false
@@ -56,6 +88,8 @@ func CheckVersion(disk string) bool {
 	if len(data) < 3 {
 		return false
 	}
+
+	data[0] &= 0b01111111
 
 	for i, v := range version {
 		if data[i] != v {
