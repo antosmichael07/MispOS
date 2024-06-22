93b683901fefc513c23b0fbb3d9414222a8f6be3
Antoš Michael <antosmichael07@gmail.com>
2024-06-22 08:47:42 +0200

    division and multiplication

--- mispasm/masm-vm/instructions.go
+++ mispasm/masm-vm/instructions.go
@@ -4,9 +4,11 @@ const (
 	ADD = iota + 1
 	SUB
 	CAL
+	MUL
+	DIV
 )
 
-var arg_sizes = []byte{0, 2, 2, 1}
+var arg_sizes = []byte{0, 2, 2, 1, 2, 2}
 
 var instructions = make(map[byte]func([]byte, []byte))
 
@@ -76,6 +78,66 @@ func init_instructions(funcs *map[string][]byte) {
 			panic("Invalid argument type for CAL\n")
 		} else {
 			run_function((*funcs)[string(arg1[1:len(arg1)-1])])
+		}
+	}
+	instructions[MUL] = func(arg1 []byte, arg2 []byte) {
+		if arg1[0] < byte(INT8) || arg2[0] < byte(INT8) || arg1[0] > byte(FLOAT64) || arg2[0] > byte(FLOAT64) {
+			panic("Invalid arguments for MUL\n")
+		} else if arg1[0] != arg2[0] {
+			panic("Arguments for MUL are different types\n")
+		} else {
+			switch arg1[0] {
+			case byte(INT8):
+				registers["rbi0"] = ByteToInt8(arg1[1]) * ByteToInt8(arg2[1])
+			case byte(INT16):
+				registers["rsi0"] = BytesToInt16(arg1[1:]) * BytesToInt16(arg2[1:])
+			case byte(INT32):
+				registers["rli0"] = BytesToInt32(arg1[1:]) * BytesToInt32(arg2[1:])
+			case byte(INT64):
+				registers["rlli0"] = BytesToInt64(arg1[1:]) * BytesToInt64(arg2[1:])
+			case byte(UINT8):
+				registers["rbui0"] = ByteToUint8(arg1[1]) * ByteToUint8(arg2[1])
+			case byte(UINT16):
+				registers["rsui0"] = BytesToUint16(arg1[1:]) * BytesToUint16(arg2[1:])
+			case byte(UINT32):
+				registers["rlui0"] = BytesToUint32(arg1[1:]) * BytesToUint32(arg2[1:])
+			case byte(UINT64):
+				registers["rllui0"] = BytesToUint64(arg1[1:]) * BytesToUint64(arg2[1:])
+			case byte(FLOAT32):
+				registers["rlf0"] = BytesToFloat32(arg1[1:]) * BytesToFloat32(arg2[1:])
+			case byte(FLOAT64):
+				registers["rllf0"] = BytesToFloat64(arg1[1:]) * BytesToFloat64(arg2[1:])
+			}
+		}
+	}
+	instructions[DIV] = func(arg1 []byte, arg2 []byte) {
+		if arg1[0] < byte(INT8) || arg2[0] < byte(INT8) || arg1[0] > byte(FLOAT64) || arg2[0] > byte(FLOAT64) {
+			panic("Invalid arguments for DIV\n")
+		} else if arg1[0] != arg2[0] {
+			panic("Arguments for DIV are different types\n")
+		} else {
+			switch arg1[0] {
+			case byte(INT8):
+				registers["rbi0"] = ByteToInt8(arg1[1]) / ByteToInt8(arg2[1])
+			case byte(INT16):
+				registers["rsi0"] = BytesToInt16(arg1[1:]) / BytesToInt16(arg2[1:])
+			case byte(INT32):
+				registers["rli0"] = BytesToInt32(arg1[1:]) / BytesToInt32(arg2[1:])
+			case byte(INT64):
+				registers["rlli0"] = BytesToInt64(arg1[1:]) / BytesToInt64(arg2[1:])
+			case byte(UINT8):
+				registers["rbui0"] = ByteToUint8(arg1[1]) / ByteToUint8(arg2[1])
+			case byte(UINT16):
+				registers["rsui0"] = BytesToUint16(arg1[1:]) / BytesToUint16(arg2[1:])
+			case byte(UINT32):
+				registers["rlui0"] = BytesToUint32(arg1[1:]) / BytesToUint32(arg2[1:])
+			case byte(UINT64):
+				registers["rllui0"] = BytesToUint64(arg1[1:]) / BytesToUint64(arg2[1:])
+			case byte(FLOAT32):
+				registers["rlf0"] = BytesToFloat32(arg1[1:]) / BytesToFloat32(arg2[1:])
+			case byte(FLOAT64):
+				registers["rllf0"] = BytesToFloat64(arg1[1:]) / BytesToFloat64(arg2[1:])
+			}
 		}
 	}
 }
