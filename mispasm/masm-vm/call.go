8f7b1da112d0e6645e8f33d37c42e31b8ae2cdb5
Antoš Michael <antosmichael07@gmail.com>
2024-06-22 10:58:57 +0200

    func print_stack

--- mispasm/masm-vm/call.go
+++ mispasm/masm-vm/call.go
@@ -10,8 +10,12 @@ var calls = make(map[int]func([]byte, []byte))
 
 func init_calls() {
 	calls[PRINTF] = func(arg1 []byte, arg2 []byte) {
-		for _, v := range stack {
-			fmt.Printf("%v", v)
-		}
+		print_stack()
+	}
+}
+
+func print_stack() {
+	for _, v := range stack {
+		fmt.Printf("%v\n", v)
 	}
 }
